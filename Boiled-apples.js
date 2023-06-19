3fb467b02154531df9e3d9bb41abe46bf2a26ad3
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-19 11:24:43 -0700

    Create Boiled-apples.js

---
+++ Boiled-apples.js
@@ -0,0 +1,27 @@
+/*
+You are the greatest chef on earth. No one boils apples like you! 
+Your restaurant is always full of guests, who love your boiled apples. 
+However when there is a greater order of boiled apples, you need some time, 
+because you have only one pot for your job. How much time do you need?
+
+Your Task:
+  Implement a function, 
+  which takes a non-negative integer, 
+  representing the number of apples to boil. 
+  It must return the time in minutes (integer), 
+  which it takes to have all the apples boiled.
+
+Rules:
+  You can put at most 8 apples into the pot at once.
+  It takes 5 minutes to boil an apple.
+  We assume, that the water is boiling all the time (no time to heat up).
+  For simplicity, we also don't consider the time it takes 
+  to put apples into the pot or get them out of it.
+
+Example:
+  0 --> 0
+  5 --> 5
+  10 --> 10
+*/
+
+
